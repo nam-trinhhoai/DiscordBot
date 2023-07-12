f5c64830344b0220b4d0770acce37784d3ecf5e3
nam-trinhhoai <nam.trinhhoai@ncc.asia>
2023-07-12 08:45:16 +0700

    pull

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 require('dotenv').config();
 const{Client , IntentsBitField} = require('discord.js');
-
+const todoList = `có 2 việc cần làm 
+1. hiểu discord event 
+2. thiết kế flow data và logic 
+cần làm cái đấy trước khi code 
+discord event thì gồm có: 
+1. text event (mention, link, emo...) 
+2. media event 
+3. game sdk event`;
 const client = new Client({
     intents: [
         IntentsBitField.Flags.Guilds,
@@ -20,6 +27,12 @@ client.on('messageCreate', (msg)=>{
     }
     if(msg.content === "hello"){
         msg.reply("meo meo");
+    }
+    if(msg.content === "very 🙂"){
+        msg.reply("Fuck You");
+    }
+    if(msg.content === "todo"){
+        msg.reply(todoList);
     }
 })
 
